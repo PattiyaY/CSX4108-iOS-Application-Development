bae2eb002eff44eb6749df3913bb5f01e9f01019
PattiyaY <pattiya.2547@icloud.com>
2024-06-22 22:23:31 +0700

    Add Class03-layout directory

---
+++ Class03-Layout/Class03-Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Class03-Layout
+//
+//  Created by Pattiya Yiadram on 22/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
